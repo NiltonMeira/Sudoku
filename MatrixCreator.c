c25ef325765d44030486fe2d17c0bde5da236d8a
Joao <joaoklippel.senai@gmail.com>
2023-11-06 11:16:19 -0300

    MatrixCreator changed again

--- MatrixCreator.c
+++ MatrixCreator.c
@@ -8,11 +8,23 @@ typedef struct {
     int** data;
 } Matrix;
 
+int generate_num(){
+    int my_num;
+
+}
+
 Matrix* createMatrix(int rows, int columns) {
     Matrix* matrix = (Matrix*)malloc(sizeof(Matrix));
     matrix->rows = rows;
     matrix->columns = columns;
 
+    int hat[9];        // pool of numbers
+    size_t n = 9;      // size of pool
+    size_t k;
+
+    srand(time(NULL));
+
+    for (k = 0; k < n; k++) hat[k] = k;
     // Allocate memory for the matrix data
     matrix->data = (int**)malloc(rows * sizeof(int*));
     for (int i = 0; i < rows; i++) {
@@ -22,12 +34,62 @@ Matrix* createMatrix(int rows, int columns) {
     // Initialize the matrix elements (you can do this as needed)
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            matrix->data[i][j] = 0; // Initialize to some default value
+            for (k = 0; k < 1; k++) {
+                size_t pick = rand() % n;
+                matrix->data[i][j] = hat[pick]+1; // Initialize to some default value
+                hat[pick] = hat[--n];
+            }
         }
     }
 
     return matrix;
 }
+
+// int **create_matrix()
+// {   
+//   // allocate Rows rows, each row is a pointer to int
+//     int **initial_matrix = (int **)malloc(3 * sizeof(int *)); 
+//     int row;
+
+//     // for each row allocate Cols ints
+//     for (row = 0; row < 3; row++) {
+//         initial_matrix[row] = (int *)malloc(3 * sizeof(int));
+//     }
+
+
+//     int numbers[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+//     srand(time(NULL));
+
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {   
+//             loop: ;
+//             int aleatory_number = (rand() % 9) + 1;
+//             int check = 0;
+        
+
+//             for (int k = 0; k < 9; k++)
+//             {
+//                 if (aleatory_number == numbers[k])
+//                 {
+//                     initial_matrix[i][j] = aleatory_number;
+//                     numbers[aleatory_number - 1] = 10;
+//                     check = 1;
+//                 }
+//             }
+
+//             if (check == 0)
+//             {
+//                 goto loop;
+//             }
+//         }
+//     }  
+
+
+//     return initial_matrix;
+// }
 
 void destroyMatrix(Matrix* matrix) {
     for (int i = 0; i < matrix->rows; i++) {
