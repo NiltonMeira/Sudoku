b41d9e87594b026039bdba6318a7182de03f86c0
Joao <joaoklippel.senai@gmail.com>
2023-11-07 19:05:12 -0300

    Array 81 fixed

--- TesteArray.c
+++ TesteArray.c
@@ -20,29 +20,52 @@ void shuffle(int *array, size_t n)
 int main(){
     srand(time(NULL));
 
-    int array_hor[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int array_ver[9];
+    // int array_hor[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    for(int j = 0; j < 9; j++){
+    // for(int j = 0; j < 9; j++){
+    //     shuffle(array_hor, 9);
+    //     if(j % 3 == 0){
+    //         printf("\n");
+    //     }
+    //     for(int i = 0; i < 9; i++){
+    //         if(i % 3 == 0){
+    //             printf("  ");
+    //         }
+    //         printf("%d  ", array_hor[i]);
+    //     }
+    //     printf("\n");
+    // }
+
+    int array_full[81];
+
+    for(int i = 0; i < 9; i++)
+    {
+        int array_hor[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         shuffle(array_hor, 9);
-        if(j % 3 == 0){
+        
+        for(int j = 0; j < 9; j++)
+        {
+            array_full[i*9+j] = array_hor[j];
+        }
+    }
+
+    for(int i = 0; i < 9; i++)
+    {
+        printf("\n");
+        if(i % 3 == 0){
             printf("\n");
         }
-        for(int i = 0; i < 9; i++){
-            if(i % 3 == 0){
+        for(int j = 0; j < 9; j++)
+        {
+            if(j % 3 == 0){
                 printf("  ");
             }
-            printf("%d  ", array_hor[i]);
+            printf("%d  ", array_full[i*9+j]);
         }
-        printf("\n");
-        //for(int k = 1; k < 10; k++){
-            array_ver[j] = array_hor[j];
-        //}
     }
-
-    for(int i = 0; i < 9; i++){
-        printf("%d  ", array_ver[i]);
-    }
+    
+    //acessar pela coluna array_full[(linha-1)*9 + coluna-1]
+    printf("%d", array_full[(3-1)*9+1-1]);
 
     return 0;
 }
